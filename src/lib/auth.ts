95206f095d5b61f702b4e12b02dfdd9fc64b6273
Muhammad Ardiansyah Asrifah_H071221068 <113808797+ArdiansyahAsrifah@users.noreply.github.com>
2026-05-03 16:10:36 +0700

    add auth for streak

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -40,7 +40,7 @@ export function getAuthorizationUrl(
   url.searchParams.set("redirect_uri", getRedirectUri());
   url.searchParams.set(
     "scope",
-    "openid offline_access user collection bookmark"
+    "openid offline_access user collection bookmark streak activity_day"
   );
   url.searchParams.set("state", state);
   url.searchParams.set("nonce", nonce);
